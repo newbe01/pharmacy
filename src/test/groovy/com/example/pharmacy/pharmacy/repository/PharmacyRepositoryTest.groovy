45454218302dd79386270dd7182144efd72a300d
newbe01 <pkwaa3@gmail.com>
2023-06-05 23:08:05 +0900

    Jpa Auditing jpa auditing 기능을 이용해 공통field 추가

--- src/test/groovy/com/example/pharmacy/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/example/pharmacy/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -4,6 +4,8 @@ import com.example.pharmacy.AbstractIntegrationContainerBaseTest
 import com.example.pharmacy.pharmacy.entity.Pharmacy
 import org.springframework.beans.factory.annotation.Autowired
 
+import java.time.LocalDateTime
+
 class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
 
     @Autowired
@@ -59,9 +61,26 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
         then:
         result.size() == 1
 
+    }
 
+    def "BaseTimeEntity 등록"() {
+        given:
+        LocalDateTime now = LocalDateTime.now()
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
 
+        def entity = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .build()
 
+        when:
+        pharmacyRepository.save(entity)
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.get(0).getCreatedDate().isAfter(now)
+        result.get(0).getModifiedDate().isAfter(now)
 
     }
 
